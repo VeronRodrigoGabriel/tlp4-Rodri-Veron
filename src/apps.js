ee67fffb75f38ae0c655f21d56d4ae6355edad8d
Rodri.V <veronrodrigo98@gmail.com>
2023-06-13 22:48:45 -0300

    Se cambio el puerto

--- src/apps.js
+++ src/apps.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 
 //Puerto del servidor
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 6000
 
 
 //Configuramos los middleware y express.json
